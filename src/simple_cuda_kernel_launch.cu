81daf0451d78da101d215a1feeed8b1e23671b9d
evelynmitchell <efmphone@gmail.com>
2023-12-04 14:22:34 -0700

    nv version kernel launc

--- src/simple_cuda_kernel_launch.cu
+++ src/simple_cuda_kernel_launch.cu
@@ -2,10 +2,9 @@
 #include <math.h>
 
 // CUDA Kernel function to add the elements of two arrays on the GPU
-// <<< (gridsize), (blocksize) >>>
-// <<<1,1>>> means 1 block with 1 thread
+
 __global__
-void add<<<1,1>>>(int n, float *x, float *y)
+void add(int n, float *x, float *y)
 {
   for (int i = 0; i < n; i++)
       y[i] = x[i] + y[i];
@@ -24,8 +23,10 @@ int main(void)
     y[i] = 2.0f;
   }
 
-  // Run kernel on 1M elements on the CPU
-  add(N, x, y);
+  // Run kernel on 1M elements on the GPU
+  // <<< (gridsize), (blocksize) >>>
+  // <<<1,1>>> means 1 block with 1 thread
+  add<<<1,1>>>(N, x, y);
 
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
@@ -37,5 +38,47 @@ int main(void)
   delete [] x;
   delete [] y;
 
+  return 0;
+}#include <iostream>
+#include <math.h>
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+    y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<20;
+  float *x, *y;
+
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run kernel on 1M elements on the GPU
+  add<<<1, 1>>>(N, x, y);
+
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  
   return 0;
 }
